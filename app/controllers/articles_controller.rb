15ffa12e742129a31c05983def00eb35e9ab0f91
milesclarkdev <milesclarkdev@gmail.com>
2023-07-31 22:52:13 -0400

    articles stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,15 @@ class ArticlesController < ApplicationController
 
     # render articles as json
     #
-    # render json: @articles, each_serializer: ArticleSerializer, adapter: :json_api
+    render json: @articles, each_serializer: ArticleSerializer, adapter: :json_api
+
+
+  end
+
+  # GET /articles/:id
+  def show
+    @article = Article.find(params[:id])
+    render json: @article, serializer: ArticleSerializer, adapter: :json_api
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"\
   root "article#index"
-  get "/articles", to: "articles#index"
+  resources :articles
 end
